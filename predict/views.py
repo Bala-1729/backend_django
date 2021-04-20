0e0948d74b2c44295a49a540f76f5144640a0af8
Balasubramaniam A <63883008+Bala-1729@users.noreply.github.com>
2021-04-20 17:02:19 +0530

    model-1 added

--- predict/views.py
+++ predict/views.py
@@ -10,12 +10,15 @@ from twilio.rest import Client
 crops=['wheat','mungbean','Tea','millet','maize','lentil','jute','cofee','cotton','ground nut','peas','rubber','sugarcane','tobacco','kidney beans','moth beans','coconut','blackgram','adzuki beans','pigeon peas','chick peas','banana','grapes','apple','mango','muskmelon','orange','papaya','watermelon','pomegranate']
 
 model=pickle.load(open('model.pkl','rb'));
+model1=pickle.load(open('model1.pkl','rb'))
 
 class PredictCrop(APIView):
     def post(self, request):
         obj=self.request.data
         serializer = CropsHistorySerializer(data=self.request.data)
         data = [obj[i] for i in obj]
+        if "" in data:
+            data = data[:4]
         output=predictor(data)
         if serializer.is_valid():
             serializer.save(user=self.request.user,crop=output)
@@ -34,5 +37,5 @@ class CropsHistory(APIView):
 def predictor(data):
     x_test = data
     sample = np.array(x_test).reshape(1,-1)
-    prediction=model.predict(sample)
+    prediction=model.predict(sample) if len(data)==4 else model1.predict(sample)
     return prediction[0]
